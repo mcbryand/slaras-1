f047c2f37c966f7d1fb04d174c11ca6e775d1e6f
mcbryand <61713663+mcbryand@users.noreply.github.com>
2025-05-29 16:46:22 +0700

    Delete sla-webapp-full directory

--- sla-webapp-full/src/pages/EstimasiHargaPasar.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function EstimasiHargaPasar() { return <div className='p-6'>Halaman Estimasi Harga Pasar Agunan</div>; }
--- sla-webapp-full/src/pages/MigrasiKPR.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function MigrasiKPR() { return <div className='p-6'>Halaman Migrasi KPR</div>; }
